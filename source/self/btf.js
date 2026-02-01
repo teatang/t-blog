e8e078e4032c4e2dfc0168edd1f744d53ae00006
tea tang <534427931@qq.com>
2026-02-01 15:40:57 +0800

    update js

--- source/self/btf.js
+++ source/self/btf.js
@@ -1,24 +1,26 @@
 function disableAllSvgWheelEvents() {
   // 获取页面中所有svg元素
   const svgElements = document.getElementsByTagName("svg");
+  console.log(svgElements, svgElements.length);
 
   // 遍历所有svg元素
   for (let i = 0; i < svgElements.length; i++) {
     const svg = svgElements[i];
 
     // 为svg元素移除wheel事件监听器
-    svg.removeEventListener("wheel", handleWheelEvent, true);
+    svg.addEventListener("wheel", handleWheelEvent);
 
     // 为svg内部所有元素移除wheel事件监听器
     const allElements = svg.querySelectorAll("*");
     allElements.forEach((element) => {
-      element.removeEventListener("wheel", handleWheelEvent, true);
+      element.addEventListener("wheel", handleWheelEvent);
     });
   }
 }
 
 // 事件处理函数（可选，用于移除已存在的监听器）
 function handleWheelEvent(event) {
+  console.log("add event listen: " + event);
   // 阻止wheel事件的默认行为
   event.preventDefault();
   // 阻止事件冒泡
@@ -49,6 +51,9 @@ function handleWheelEvent(event) {
     : document.querySelectorAll('a[href^="/en/"]');
 
   eventFn(selector, nowIncludeEN);
-
-  disableAllSvgWheelEvents();
 })();
+
+window.onload = function () {
+  console.log("页面加载完成！");
+  disableAllSvgWheelEvents();
+};
